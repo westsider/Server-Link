b79ac62467eb891ec4053c7c195f0e9c8bd3e976
Warren Hansen <whansen1@mac.com>
2017-10-16 17:49:31 -0700

    Initial Commit

---
+++ Server Link/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Server Link
+//
+//  Created by Warren Hansen on 10/16/17.
+//  Copyright © 2017 Warren Hansen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
